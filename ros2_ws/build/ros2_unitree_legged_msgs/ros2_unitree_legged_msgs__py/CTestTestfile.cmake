b30d8ebbc1c7aeff4383e59676d290d19c9a28da
Shivayogi Akki <sakki@mtu.edu>
2023-03-06 13:43:46 -0500

    update

---
+++ ros2_ws/build/ros2_unitree_legged_msgs/ros2_unitree_legged_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leggedrobot/Documents/GitHub/unitree-go1-ros/ros2_ws/build/ros2_unitree_legged_msgs/ros2_unitree_legged_msgs__py
+# Build directory: /home/leggedrobot/Documents/GitHub/unitree-go1-ros/ros2_ws/build/ros2_unitree_legged_msgs/ros2_unitree_legged_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ros2_ws/build/unitree_legged_sdk/CMakeFiles/example_joystick.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_joystick.dir/example/example_joystick.cpp.o"
+  "CMakeFiles/example_joystick.dir/example/example_joystick.cpp.o.d"
+  "example_joystick"
+  "example_joystick.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example_joystick.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
